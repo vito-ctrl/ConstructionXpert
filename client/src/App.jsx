b02c03669614187a1463d3ee1988ad251ca84fe2
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2025-03-17 12:32:31 +0000

    Initialize vite, Install tailwind, Set up folder structure (components, pages, services)

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Projects from './pages/Projects'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Projects/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
